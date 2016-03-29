94366f38262e99986bf8f910acb9a36827214121
leewaygroups <leewaygroups@gmail.com>
2016-03-30 04:54:30 +0700

    Test for usecase: Array of connections passed in as configs

--- test/test.js
+++ test/test.js
@@ -25,11 +25,31 @@ var configs = [
     },
 ]
 
+//var arrayConnections = mysqlAll();
+//var arrayConnections = mysqlAll(configs)
 //var arrayConnections = mysqlAll(configs, 'array');
 
 
+//array of configs test
 describe('mysqlAll', function() {
-  it('mysqlAll() should throw an exception if no item is passed in', function() {
-    expect(mysqlAll()).to.Throw('connection object invalid');
+  it('should return an object if no return choice/option is passed', function() {
+    expect(mysqlAll(configs)).to.be.an('object');
+  });
+  
+  it('should return an object if "object" is passed as return choice/option', function() {
+    expect(mysqlAll(configs, 'object')).to.be.an('object');
+  });
+  
+  it('should return an object with ownProperties host+database', function(){
+      expect(mysqlAll(configs)).have.ownProperty('localhostmy_db1');
+      expect(mysqlAll(configs)).have.ownProperty('somehostmy_db2');
+      expect(mysqlAll(configs)).have.ownProperty('anotherhostmy_db2');
+  });
+});
+
+//should return an array if "array" is passed as return choice/option
+describe('mysqlAll: Array configs test', function() {
+  it('should return an array if "array" is passed as return choice/option', function() {
+    expect(mysqlAll(configs, 'array')).to.be.an('array');
   });
 });
